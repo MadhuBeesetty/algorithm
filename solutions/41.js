6aebc882882da8b99cf766d0972b1fe7e2163ad9
Madhu <madhusudhan9kings@gmail.com>
2017-10-12 21:43:11 -0700

    prime numbers between0-100

---
+++ solutions/41.js
@@ -0,0 +1,22 @@
+const prime = (limit,a=2,b=[])=>{
+  if(a==limit){
+    console.log(b);
+    return b;
+  }
+  const prime1 = (i=2)=>{
+    if(a%i==0){
+      if(a==i){
+        b.push(a);
+        return;
+      }else{
+        return ;
+      }
+    }
+    return prime1(i+1);
+  }
+  prime1();
+  return prime(limit,a+1,b);
+}
+prime(200);
+
+
