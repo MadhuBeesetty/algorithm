31b8812cf027f5273255f335d3a7154e174603e5
Madhu <madhusudhan9kings@gmail.com>
2017-10-12 17:11:29 -0700

    finding all divisors of a number

--- solutions/5.js
+++
@@ -1,8 +0,0 @@
-const funct = (num,fun,i=0)=>{
-  if(i<num){
-    fun();
-    funct(num,fun,i+1)
-  }
-}
-
-module.exports = funct;
--- test/7.js
+++
@@ -1,9 +0,0 @@
-const gcd =require('../solutions/7.js')
-const test=(a,b,result)=>{
-  if (gcd(a,b)==result){
-    console.log("your programme looks good");
-  }else{
-    console.log("your programme looks bad");
-  }
-}
-test(15,6,3)
