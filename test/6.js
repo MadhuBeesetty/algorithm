0bd4caf45b798213213d86199da555ea37da0ccb
Madhu <madhusudhan9kings@gmail.com>
2017-10-16 13:33:56 -0700

    calling a function x number of time

--- test/6.js
+++ test/6.js
@@ -1,20 +1,18 @@
 const callx = require('../solutions/6.js')
 const test = (num)=>{
   let count =0;
-  let passng = true;
   const hello= (a)=>{
     count=count+1;
-    if(a!=count){
-      passing=false;
-    }
   }
   callx(hello,num);
-  if(count==num && passng==true){
+  if(count==num){
     console.log('correct');
   }else{
     console.log('wrong');
   }
 }
 test(3);
+test(4);
+test(5);
 
 
