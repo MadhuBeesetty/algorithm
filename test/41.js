161f5ce40d1ee4fed2d0ce18f8fe33350d28d32b
Madhu <madhusudhan9kings@gmail.com>
2017-10-16 13:19:59 -0700

    testing all prime numbers in a range

---
+++ test/41.js
@@ -0,0 +1,24 @@
+const testing = require('../solutions/41.js');
+const test = (num)=>{
+  let a = testing(num);
+  const test1 = (i=0) =>{
+    const test2 = (j=2) =>{
+      if(a[i]%j == 0){
+        if(i == j){
+          return;
+        }
+      }
+        return test2(j+1);
+    }
+    if( i == a.length-1 ){
+      console.log("your programme is good");
+      return ;
+  }
+    return test1(i+1);
+  }
+  test1();
+}
+test(100);
+
+
+
