c1a12026ed9725a20276efdf5740a07f7936d212
Madhu <madhusudhan9kings@gmail.com>
2017-10-17 17:06:53 -0700

    conversion of camel case to kebab case

--- solutions/45.js
+++ solutions/45.js
@@ -1,15 +1,13 @@
+//conversion of camel case to kebab case//
 const kebab = (a,i=0,b=[])=>{
   if(i == a.length){
     b=b.join('');
     return b;
   }
-  if(a[i]!=a[i].toUpperCase()){
-    b.push(a[i]);
-  }else{
-    c=a[i].toLowerCase();
-    b.push("-");
-    b.push(c);
-  }
+  if(a[i]==a[i].toUpperCase()){
+    b.push("-")
+  };
+  b.push(a[i].toLowerCase());
 return kebab(a,i+1,b)
 }
 module.exports=kebab
